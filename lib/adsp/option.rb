b04bfd08d89a8aa6df512cd024567cf21f5b739e
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-06-07 11:31:04 +0300

    added lib option

---
+++ lib/adsp/option.rb
@@ -0,0 +1,45 @@
+# Abstract data stream processor.
+# Copyright (c) 2021 AUTHORS, MIT License.
+
+require_relative "error"
+require_relative "validation"
+
+module ADSP
+  class Option
+    DEFAULT_BUFFER_LENGTH = 0
+
+    def self.get_compressor_options(options, buffer_length_names)
+      Validation.validate_hash options
+      Validation.validate_array buffer_length_names
+
+      buffer_length_names.each { |name| Validation.validate_string name }
+
+      buffer_length_defaults = buffer_length_names.each_with_object({}) do |name, defaults|
+        defaults[name] = DEFAULT_BUFFER_LENGTH
+      end
+
+      options = buffer_length_defaults.merge options
+
+      buffer_length_names.each { |name| Validation.validate_not_negative_integer options[name] }
+
+      options
+    end
+
+    def self.get_decompressor_options(options, buffer_length_names)
+      Validation.validate_hash options
+      Validation.validate_array buffer_length_names
+
+      buffer_length_names.each { |name| Validation.validate_string name }
+
+      buffer_length_defaults = buffer_length_names.each_with_object({}) do |name, defaults|
+        defaults[name] = DEFAULT_BUFFER_LENGTH
+      end
+
+      options = buffer_length_defaults.merge options
+
+      buffer_length_names.each { |name| Validation.validate_not_negative_integer options[name] }
+
+      options
+    end
+  end
+end
